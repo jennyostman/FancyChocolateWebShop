7e14423bef6fbbfb7af26db72ea0fc875831b7f3
Jenny Östman <jenny.ostman@yh.nackademin.se>
2019-04-24 11:16:06 +0200

    The beginning

--- FancyChocolateWebShop-ejb/src/java/test/TestSessionBean.java
+++ FancyChocolateWebShop-ejb/src/java/test/TestSessionBean.java
@@ -14,6 +14,5 @@ import javax.ejb.LocalBean;
 @LocalBean
 public class TestSessionBean {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    // TestBöna
 }
