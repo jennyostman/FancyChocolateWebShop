6ba45b58427c37f81965c7385fc8b45b38d4cca7
Jenny Östman <jenny.ostman@yh.nackademin.se>
2019-04-24 10:53:54 +0200

    The beginning

---
+++ FancyChocolateWebShop-ejb/src/java/test/TestSessionBean.java
@@ -0,0 +1,19 @@
+/*
+ * Javautveckling 2018
+ */
+package test;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Jenny
+ */
+@Stateless
+@LocalBean
+public class TestSessionBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
