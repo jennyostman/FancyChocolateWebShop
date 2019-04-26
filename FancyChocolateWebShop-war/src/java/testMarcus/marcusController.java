72fd9acbbd035585f1bacc44ba91b2a2051f3628
Marcstro <marcus.strom.90@gmail.com>
2019-04-26 10:52:25 +0200

    Produktlistan visar nu objekt,  kan andra objekten i MarcusController

--- FancyChocolateWebShop-war/src/java/testMarcus/marcusController.java
+++ FancyChocolateWebShop-war/src/java/testMarcus/marcusController.java
@@ -8,6 +8,8 @@ package testMarcus;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  *
@@ -17,10 +19,35 @@ import java.io.Serializable;
 @SessionScoped
 public class marcusController implements Serializable {
 
+    private ArrayList<ChokladProdukt> ProductList = new ArrayList();
+    
     /**
      * Creates a new instance of marcusController
      */
-    public marcusController() {
+//    public marcusController() {
+//    }
+    
+    public marcusController(){
+        System.out.println("test2");
+        //ProductList = new ArrayList();
+        ChokladProdukt ck1 = new ChokladProdukt("\"marabo\"",19.99, 1);
+        ChokladProdukt ck2 = new ChokladProdukt("\"chokladsvan\"",399, 2);
+        ChokladProdukt ck3 = new ChokladProdukt("\"chokladkex\"",4200, 3);
+        ProductList.add(ck1);
+        ProductList.add(ck2);
+        ProductList.add(ck3);
+        
     }
+
+    public ArrayList<ChokladProdukt> getProductList() {
+        return ProductList;
+    }
+
+    public void setProductList(ArrayList<ChokladProdukt> ProductList) {
+        this.ProductList = ProductList;
+    }
+
+   
+    
     
 }
