6f6b8e68e2c0151f554a2f7aa2ce9109d1f9e496
Jenny Östman <jenny.ostman@yh.nackademin.se>
2019-04-26 12:33:46 +0200

    Remove from cart possible

--- FancyChocolateWebShop-war/src/java/test/CartDbStandIn.java
+++ FancyChocolateWebShop-war/src/java/test/CartDbStandIn.java
@@ -1,18 +1,16 @@
 
 package test;
 
-import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import java.util.ArrayList;
+import java.util.List;
 
 
-@Named(value = "cartDbStandIn")
-@Dependent
 public class CartDbStandIn {
-
+    
 private String chocolateName, pictureName;
     private float price;
     private int amount;
-    
+    private List<CartDbStandIn> cartContent = null;
     
     public CartDbStandIn(){}
     
@@ -54,5 +52,37 @@ private String chocolateName, pictureName;
     public void setAmount(int amount) {
         this.amount = amount;
     }
+
+    public List<CartDbStandIn> getCartContent() {
+        return cartContent;
+    }
+
+    public void setCartContent(List<CartDbStandIn> cartContent) {
+        this.cartContent = cartContent;
+    }
     
+    // Att labba med tills vidare:
+    public List<CartDbStandIn> createCartObjects(){
+        cartContent = new ArrayList(){};
+        cartContent.add(new CartDbStandIn("Grand-Cru-by-Pierre-Marcolni-1.jpg", "Grand Cru by Pierre Marcolni", 102.50f, 3));
+        cartContent.add(new CartDbStandIn("Richart-Chocolate.jpg", 
+                "Richart Chocolate", 115.0f, 10));
+        cartContent.add(new CartDbStandIn("Chocolates-with-Edible-Gold-by-DeLafee.jpg", 
+                "Chocolates with Edible Gold by DeLafee", 508.0f, 1));
+        // this.cartContent = cartContent;
+        return cartContent;
+    }
+    
+    
+    
+    public boolean deleteFromCart(CartDbStandIn chocolateObj, List<CartDbStandIn> cart){
+        boolean delete = false;
+        int size = cart.size();
+        cart.remove(chocolateObj);
+        // Koll om något tagits bort
+        if(size > cart.size()){
+            delete = true;
+        }
+        return delete;
+    }
 }
